7d39044fe88b5004849579129bd0923f5674b69a
Haicx98 <35750406+Haicx98@users.noreply.github.com>
2021-08-25 08:45:48 +0700

    project 1

---
+++ js/product.js
@@ -0,0 +1,27 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+    123: {
+        name : "Adult ticket",
+        desc : "1 adult ticket.",
+        img : "ticket.jpg",
+        price : 100
+    },
+    124: {
+        name : "Children ticket",
+        desc : "1 children ticket.",
+        img : "ticket.jpg",
+        price : 15
+    },
+    125: {
+        name : "Combo ticket",
+        desc : "All difficulty unreserved the solicitude.",
+        img : "ticket.jpg",
+        price : 150
+    },
+    126: {
+        name : "Family combo ticket",
+        desc : "2 adults 2 children ticket and free foods ",
+        img : "ticket.jpg",
+        price : 500
+    }
+};
